fce69791002fd2fbe89114229d01bca39b8cafac
Elias Olate <43555290+bliss-21@users.noreply.github.com>
2023-05-22 23:44:05 -0400

    Update init.py

--- automationSortFolderDownloads/init.py
+++ automationSortFolderDownloads/init.py
@@ -2,89 +2,88 @@ import os
 import glob
 import shutil
 
-ruta_destino = os.path.join(os.path.expanduser('~'), 'MisDescargas')
-ruta_origen =os.path.join(os.path.expanduser('~'), 'Downloads')
+"""
+init variables
+"""
 
-img = ['JPEG','JPG','PNG','GIF','BMP','TIFF','SVG','WebP']
-docs = ['DOC','DOCX','PDF','TXT','XLS','XLSX','PPT','PPTX','CSV','RTF','ODT']
-audio = ['MP3','WAV','FLAC','AAC','OGG','M4A','AIFF']
-video = ['MP4','AVI','MOV','MKV','WMV','FLV','MPEG','webm']
-programs = ['exe','msi']
-compressed = ['zip', 'rar','7z']
+DESTINATION_FOLDER_NAME ="MisDescargas2sss"
+DESTINATION_PATH = os.path.join(os.path.expanduser('~'), DESTINATION_FOLDER_NAME)
+SOURCE_PATH =os.path.join(os.path.expanduser('~'), 'Downloads')
 
-delete = ['crdownload',]
+TO_REMOVE_EXTENSIONS = ['crdownload',]
 
-folder_tree = {
-    "programs": programs,
-    "video":    video,
-    "audio":    audio,
-    "docs":     docs,
-    "img":      img,
-    "compressed":compressed,
+FOLDER_NAMES = {
+    "programs": ['exe','msi',],
+    "video":    ['MP4','AVI','MOV','MKV','WMV','FLV','MPEG','webm',],
+    "audio":    ['MP3','WAV','FLAC','AAC','OGG','M4A','AIFF',],
+    "docs":     ['DOC','DOCX','PDF','TXT','XLS','XLSX','PPT','PPTX','CSV','RTF','ODT',],
+    "img":      ['JPEG','JPG','PNG','GIF','BMP','TIFF','SVG','WebP',],
+    "compressed":['zip', 'rar','7z',],
     "folders": [],
     "others": []
 }
 
+folders = list(FOLDER_NAMES.keys())
 
-folders = list(folder_tree.keys())
+"""
+crear carpetas si no existen
+"""    
+for f in folders:
+    new_folder = f"{DESTINATION_PATH}/{f}"
+    if not os.path.exists(new_folder):
+        os.makedirs(new_folder)
 
-# Verificar si la carpeta no existe previamente
-if not os.path.exists(ruta_destino):
-    # Crear la carpeta
-    os.makedirs(ruta_destino)
-    # creamos los directorios
-    for f in folders:
-        os.makedirs(f"{ruta_destino}/{f}")
-else:
-    # validamos si existen las carpetas organizadoras dentro de la carpeta de destino, de no existir se crea
-    for f in folders:
-        folder = f"{ruta_destino}/{f}"
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-
-
-if not os.path.exists(ruta_origen):
+if not os.path.exists(SOURCE_PATH):
     raise ValueError("No se encontro la carpeta de origen")
 
-"""----------------------Listo--------------------------"""
 
+"""
+eliminarmos archivos que no interezan
+"""
+for extension in TO_REMOVE_EXTENSIONS:
+    patron_archivos = f'{SOURCE_PATH}/*.{extension.lower()}'
+    archivos_to_TO_REMOVE_EXTENSIONSd = glob.glob(patron_archivos)
+    for file in archivos_to_TO_REMOVE_EXTENSIONSd:
+        os.remove(file)
+
+"""
+movemos las carpetas
+"""
+elementos = os.listdir(SOURCE_PATH)
+
+# Filtra los elementos que son carpetas
+folders_to_moved = [elemento for elemento in elementos if os.path.isdir(os.path.join(SOURCE_PATH, elemento))]
+
+# Imprime los nombres de las carpetas
+for folder in folders_to_moved:
+    shutil.move(f"{SOURCE_PATH}/{folder}", f"{DESTINATION_PATH}/folders")
+
+"""
+mover los archivosa sus carpetas correspondientes
+"""
 for f in folders:
-    #obtenemos la lista de extenciones  para la carpeta organizadora
-    for extension in folder_tree[f]:
-        # Especifica el patrón de archivos
-        patron_archivos = f'{ruta_origen}/*.{extension.lower()}'
+    for extension in FOLDER_NAMES[f]:
+        patron_archivos = f'{SOURCE_PATH}/*.{extension.lower()}'
 
-        # Obtiene la lista de archivos que coinciden con el patrón
         archivos_to_moved = glob.glob(patron_archivos)
 
         for file in archivos_to_moved:
-            shutil.move(file, f"{ruta_destino}/{f}")
-
-
-#movemos las carpetas 
-# Obtiene la lista de elementos dentro de la carpeta principal
-elementos = os.listdir(ruta_origen)
-
-# Filtra los elementos que son carpetas
-carpetas = [elemento for elemento in elementos if os.path.isdir(os.path.join(ruta_origen, elemento))]
-
-# Imprime los nombres de las carpetas
-for carpeta in carpetas:
-    shutil.move(f"{ruta_origen}/{carpeta}", f"{ruta_destino}/folders")
-
-#Eliminamos los archios que no queremos
-for extension in delete:
-    patron_archivos = f'{ruta_origen}/*.{extension.lower()}'
-    archivos_to_deleted = glob.glob(patron_archivos)
-    for file_delete in archivos_to_deleted:
-        os.remove(file_delete)
-
-#movemos los que no calzan
-
-# Lista los archivos de la carpeta
-archivos = os.listdir(ruta_origen)
-
-# Imprime los nombres de los archivos
-for archivo in archivos:
-    if os.path.isfile(os.path.join(ruta_origen, archivo)):
-        shutil.move(f"{ruta_origen}/{archivo}", f"{ruta_destino}/others")
+            archivo_destino = os.path.join(DESTINATION_PATH, f, os.path.basename(file))
+            
+            if not os.path.exists(archivo_destino):
+                shutil.move(file, archivo_destino)
+            else:
+                # Generar un nuevo nombre de archivo único
+                nombre_base, extension = os.path.splitext(os.path.basename(file))
+                contador = 1
+                nuevo_nombre = f"{nombre_base}_{contador}{extension}"
+                nuevo_archivo_destino = os.path.join(DESTINATION_PATH, f, nuevo_nombre)
+                
+                # Incrementar el contador hasta encontrar un nombre único
+                while os.path.exists(nuevo_archivo_destino):
+                    contador += 1
+                    nuevo_nombre = f"{nombre_base}_{contador}{extension}"
+                    nuevo_archivo_destino = os.path.join(DESTINATION_PATH, f, nuevo_nombre)
+                
+                # Mover el archivo con el nuevo nombre
+                shutil.move(file, nuevo_archivo_destino)
