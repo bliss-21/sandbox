f39b83af0105ab8de2ea4400cc7f1c720b0fdd76
Elias Olate <43555290+bliss-21@users.noreply.github.com>
2023-06-15 00:30:44 -0400

    animación de Like
    Animación de un corazón al hacer doble click sobre una imagen.

---
+++ LikeAnimationJs/like.js
@@ -0,0 +1,11 @@
+const img = document.querySelector('img');
+const icon = document.querySelector('svg');
+
+img.addEventListener('dblclick', () => {
+    console.log("into event")
+    icon.classList.add('like')
+
+    setTimeout(() => {
+        icon.classList.remove('like')
+    }, 1000)
+})
